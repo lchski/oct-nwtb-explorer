7ea05145262167f95d0c65380f758bda0f9fff7a
Lucas Cherkewski <lucas@lucascherkewski.com>
2025-04-29 20:37:25 -0400

    cache bust on windows (fixes #11)
    sorry for the bandwidth, Windows friends

--- src/lib/octdb.js
+++ src/lib/octdb.js
@@ -2,9 +2,9 @@ import {FileAttachment} from "observablehq:stdlib"
 import {DuckDBClient} from "npm:@observablehq/duckdb"
 
 export const octdb = DuckDBClient.of({
-	stops: FileAttachment("../data/octranspo.com/stops_normalized.parquet"),
-	stop_times_by_stop: FileAttachment("../data/octranspo.com/stop_times_by_stop.parquet"),
-  stop_times: FileAttachment("../data/octranspo.com/stop_times.parquet"),
+	stops: FileAttachment("../data/octranspo.com/stops_normalized.parquet").href + (navigator.userAgent.includes("Windows") ? `?t=${Date.now()}` : ""),
+	stop_times_by_stop: FileAttachment("../data/octranspo.com/stop_times_by_stop.parquet").href + (navigator.userAgent.includes("Windows") ? `?t=${Date.now()}` : ""),
+	stop_times: FileAttachment("../data/octranspo.com/stop_times.parquet").href + (navigator.userAgent.includes("Windows") ? `?t=${Date.now()}` : ""),
 })
 
 export const array_to_sql_qry_array = (arr) => `['${arr.join("','")}']`
