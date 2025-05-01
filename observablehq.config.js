bcab8e9021fad35365222ffb8e0e8b78e320bee8
Lucas Cherkewski <lucas@lucascherkewski.com>
2025-05-01 08:15:39 -0400

    temporarily disable route-specific pages (#12)

--- observablehq.config.js
+++ observablehq.config.js
@@ -42,13 +42,13 @@ export default {
       ]
     }
   ],
-  async *dynamicPaths() {
-    const route_ids = await octdb.runAndReadAll(`SELECT DISTINCT route_id FROM routes`)
+  // async *dynamicPaths() {
+  //   const route_ids = await octdb.runAndReadAll(`SELECT DISTINCT route_id FROM routes`)
 
-    for await (const {route_id} of route_ids.getRowObjects()) {
-      yield `/routes/${route_id}`;
-    }
-  },
+  //   for await (const {route_id} of route_ids.getRowObjects()) {
+  //     yield `/routes/${route_id}`;
+  //   }
+  // },
 
   // Content to add to the head of the page, e.g. for a favicon:
   // head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
