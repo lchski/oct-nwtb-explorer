ff67678a9da300b706252a99d8d3c49fd268263a
Lucas Cherkewski <lucas@lucascherkewski.com>
2025-05-25 17:14:12 -0400

    quick fix for broken ward pages, swap back to geojson

--- src/lib/maps.js
+++ src/lib/maps.js
@@ -19,12 +19,17 @@ export const ons_neighbourhoods = shapefile.read(
 	]))
 )
 
-export const wards = shapefile.read(
-	...(await Promise.all([
-		FileAttachment("../data/ottawa.ca/wards_2022_to_2026.shp").stream(),
-		FileAttachment("../data/ottawa.ca/wards_2022_to_2026.dbf").stream()
-	]))
-)
+// export const wards = shapefile.read( // TODO: fix this, breaks certain ward pages
+// 	...(await Promise.all([
+// 		FileAttachment("../data/ottawa.ca/wards_2022_to_2026.shp").stream(),
+// 		FileAttachment("../data/ottawa.ca/wards_2022_to_2026.dbf").stream()
+// 	])),
+// 	{
+// 		encoding: "utf-8"
+// 	}
+// )
+
+export const wards = FileAttachment("../data/ottawa.ca/wards_2022_to_2026-optim.geojson").json()
 
 // Generated with mapshaper, merging 24 wards into one
 // ref: https://helpcenter.flourish.studio/hc/en-us/articles/8827921931919-How-to-merge-regions-with-Mapshaper
